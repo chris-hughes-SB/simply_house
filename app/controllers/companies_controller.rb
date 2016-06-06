33f99ae6857945c4bea951fc390b97739fc32307
Chris Hughes <chris.hughes@simplybusiness.com>
2016-06-06 14:32:19 +0100

    Search functionality added

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -3,5 +3,16 @@ class CompaniesController < ApplicationController
   end
 
   def search
+    @response = search_companies_by_name(:q).body
   end
+
+  private
+    def search_companies_by_name(name)
+      HTTParty.get('https://api.companieshouse.gov.uk/search/companies',
+        { query: { q: name },
+          basic_auth: { username: "M0HNR639VBSB9d8RQWXElUV1nEJ3kbRhcaQgDMzJ",
+                        password: "" }
+        }
+      )
+    end
 end
