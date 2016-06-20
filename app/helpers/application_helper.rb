9e99d8577ccbfb0839a78be8cd60c7678818e560
Chris Hughes <chris.hughes@simplybusiness.com>
2016-06-20 12:41:23 +0100

    sic code lookup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def lookup_sic_code(code)
+    CSV.read(Rails.root + "codes.csv", headers: true, header_converters: :symbol)
+      .find(-> { {description: "#{code} doesn't exist"} }) { |hash| hash[:sic_code] == code.to_s }[:description]
+  end
+
 end
